0cb052a4c72568bd6eaf0b6284e084c7b0f0c730
oleg-ostanin <oleg.alex.ostanin@gmail.com>
2021-05-19 10:18:25 +0300

    initial commit

---
+++ src/main/java/com/example/elasticsearchudemy/ElasticsearchUdemyApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchUdemyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchUdemyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/elasticsearchudemy/ElasticsearchUdemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchUdemyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
